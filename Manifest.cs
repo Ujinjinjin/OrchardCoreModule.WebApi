07298b2717c5c21554d247a367e851cc747182de
Camille Galladjov <kagallad@mts.ru>
2019-08-16 12:40:37 +0300

    Categorized module

--- Manifest.cs
+++ Manifest.cs
@@ -2,7 +2,7 @@ using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
     Name = "OrchardCore.WebApi",
-    Author = "The Orchard Team",
+    Author = "The SmartMed Team",
     Website = "http://orchardproject.net",
     Version = "0.0.1",
     Description = "OrchardCore.WebApi"
