077825158ab8e3093abe3f94002d675e646b92e6
Camille Galladjov <ujinjinjin@outlook.com>
2019-08-23 20:54:50 +0300

    next package version script

---
+++ version.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+import re
+from typing import List
+
+version_regex = r'(\d+)\.(\d+)\.(\d+)(-\w+)?'
+
+
+def get_package_info(_target_name: str) -> List[str]:
+    """Retrieve package info from nuget"""
+    out = subprocess.Popen(['nuget', 'list', f'id:{_target_name}', '-Prerelease'],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+
+    stdout, stderr = out.communicate()
+    return stdout.decode('utf8').split('\r\n')
+
+
+def get_next_version(_current_version: str) -> str:
+    """Get next version number of package"""
+    major, minor, patch, flag = re.findall(version_regex, _current_version)[0]
+    patch = int(patch) + 1
+    return f'{major}.{minor}.{patch}{flag}'
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) != 1, 'You must specify only package name in order to get it\'s latest version number'
+    target_name = sys.argv[1]
+
+    package_infos = get_package_info(target_name)
+
+    for package_info in package_infos:
+        if len(package_info) == 0:
+            continue
+        name, version = package_info.split(' ')
+        if name == target_name:
+            next_version = get_next_version(version)
+            print(next_version)
