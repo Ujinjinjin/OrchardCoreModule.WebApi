3d08b0687e5f67d23f06c7f2f6683b582d87e4a6
Camille Galladjov <ujinjinjin@outlook.com>
2019-08-24 00:33:49 +0300

    replaced python 3.6 syntax with older version

--- version.py
+++ version.py
@@ -8,7 +8,7 @@ version_regex = r'(\d+)\.(\d+)\.(\d+)(-\w+)?'
 
 def get_package_info(_target_name: str) -> List[str]:
     """Retrieve package info from nuget"""
-    out = subprocess.Popen(['nuget', 'list', f'id:{_target_name}', '-Prerelease'],
+    out = subprocess.Popen(['nuget', 'list', 'id:{0}'.format(_target_name), '-Prerelease'],
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT)
 
