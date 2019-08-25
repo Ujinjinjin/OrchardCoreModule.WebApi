6360e4888f381322224f1c0a196ccac9d2ac2f53
Camille Galladjov <ujinjinjin@outlook.com>
2019-08-25 23:22:52 +0300

    debug output in version.ps1

--- tools/version.ps1
+++ tools/version.ps1
@@ -1,5 +1,9 @@
 param([String]$ProjectName, [String]$ToolsPath, [String]$Args)
 
+Write-Output $ProjectName
+Write-Output $ToolsPath
+Write-Output $Args
+
 $newVersion = (python $ToolsPath/main.py -n $ProjectName $Args) | Out-String
 
 Write-Output "##vso[task.setvariable variable=PackageVersion;]$newVersion"
